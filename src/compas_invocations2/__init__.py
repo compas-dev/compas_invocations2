41263006c59861dd716ee409cb8e11c5d0144ea9
Licini <leochen1990@gmail.com>
2024-03-01 12:26:51 +0100

    init

--- src/compas_invocations2/__init__.py
+++ src/compas_invocations2/__init__.py
@@ -1,5 +1,5 @@
-__author__ = ["Gonzalo Casas"]
+__author__ = ["Li Chen"]
 __copyright__ = "ETH Zurich"
 __license__ = "MIT License"
-__email__ = "casas@arch.ethz.ch"
+__email__ = "li.chen@arch.ethz.ch"
 __version__ = "0.1.0"
